32dfafd3912f5d77d3f7094e3eeb502b0c2dbe4a
Irene-Busah <i.busah@alustudent.com>
2024-03-25 01:22:31 +0200

    slight changes

--- dashboard/views/consumption.py
+++ dashboard/views/consumption.py
@@ -2,13 +2,13 @@ from django.db.models import Sum
 from food_data_api.models.region_store import Region, Store
 from food_data_api.models.sales import Sale
 
+
 def get_frequently_consumed_by_region(request):
     sales_by_region_and_category = (
-        Sale.objects
-        .select_related('product__category', 'store__region')
-        .values('store__region__name', 'product__category__name')
-        .annotate(total_quantity=Sum('quantity'))
-        .order_by('store__region__name', '-total_quantity')
+        Sale.objects.select_related("product__category", "store__region")
+        .values("store__region__name", "product__category__name")
+        .annotate(total_quantity=Sum("quantity"))
+        .order_by("store__region__name", "-total_quantity")
     )
-    print(sales_by_region_and_category)
+    # print(sales_by_region_and_category)
     return list(sales_by_region_and_category)
